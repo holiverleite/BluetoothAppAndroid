14af6aa08231bc4fa39cd8f866c1f6f8b23a6f1a
Haroldo Leite <holiverleite@gmail.com>
2019-09-27 11:15:39 -0300

    Implementing bluetooth paring devices

--- app/src/main/java/com/example/holiverleite/bluetoothapp/MainActivity.kt
+++ app/src/main/java/com/example/holiverleite/bluetoothapp/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.holiverleite.bluetoothapp
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
+    var TAG = "a"
     val REQUEST_ENABLE_BT = 1
-
     var bluetoothAdapter: BluetoothAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +26,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         this.bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-
-        this.updateBluetoothStatusButton()
-
         if (bluetoothAdapter != null) {
             this.bluetoothButton.setOnClickListener {
                 this.changeBluetoothStatus()
+            }
+        }
+
+        this.updateBluetoothStatusButton()
+
+        // Device Master Button
+        this.device_master.setOnClickListener {
+            val discoverableIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+                putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
+            }
+            startActivity(discoverableIntent)
+
+
+        }
+
+        // Device 1 Button
+        this.device1.setOnClickListener {
+            val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+            registerReceiver(this.receiver, filter)
+
+            this.bluetoothAdapter?.startDiscovery()
+        }
+    }
+
+    // Device Client receiver
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val action: String = intent?.action.toString()
+            when(action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    val device: BluetoothDevice = intent!!.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    val deviceName = device.name
+                    val deviceHardwareAddress = device.address // MAC
+                }
             }
         }
     }
@@ -34,6 +73,7 @@ class MainActivity : AppCompatActivity() {
         this.updateBluetoothStatusButton()
     }
 
+    // Other Methods
     fun changeBluetoothStatus() {
         if (this.bluetoothAdapter?.isEnabled == true) {
             this.bluetoothAdapter?.disable()
@@ -51,4 +91,6 @@ class MainActivity : AppCompatActivity() {
             this.bluetoothButton.setBackgroundColor(Color.GREEN)
         }
     }
+
+    
 }
