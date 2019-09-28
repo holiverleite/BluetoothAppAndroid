8541148f857200d3cf33a20f3e9e81a05e4c02e0
Haroldo Leite <holiverleite@gmail.com>
2019-09-28 11:16:56 -0300

    -Implemented the pairing feature between two devices

--- app/src/main/java/com/example/holiverleite/bluetoothapp/MainActivity.kt
+++ app/src/main/java/com/example/holiverleite/bluetoothapp/MainActivity.kt
@@ -14,16 +14,20 @@ import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.IOException
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     var TAG = "a"
+    var UUID_APP: UUID? = null
     val REQUEST_ENABLE_BT = 1
     var bluetoothAdapter: BluetoothAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.UUID_APP = UUID.fromString("2b2e0f29-3129-4419-af47-30e1c84610aa")
 
         this.bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         if (bluetoothAdapter != null) {
@@ -41,7 +45,7 @@ class MainActivity : AppCompatActivity() {
             }
             startActivity(discoverableIntent)
 
-
+            AcceptThread().mmServerSocket
         }
 
         // Device 1 Button
@@ -62,6 +66,8 @@ class MainActivity : AppCompatActivity() {
                     val device: BluetoothDevice = intent!!.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
                     val deviceName = device.name
                     val deviceHardwareAddress = device.address // MAC
+
+                    ConnectThread(device).run()
                 }
             }
         }
@@ -92,5 +98,77 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    
+
+
+
+
+    // Server
+    private inner class AcceptThread : Thread() {
+
+        val mmServerSocket: BluetoothServerSocket? by lazy(LazyThreadSafetyMode.NONE) {
+            bluetoothAdapter?.listenUsingInsecureRfcommWithServiceRecord("11", UUID_APP)
+        }
+
+        override fun run() {
+            // Keep listening until exception occurs or a socket is returned.
+            var shouldLoop = true
+            while (shouldLoop) {
+                val socket: BluetoothSocket? = try {
+                    mmServerSocket?.accept()
+                } catch (e: IOException) {
+                    Log.e(TAG, "Socket's accept() method failed", e)
+                    shouldLoop = false
+                    null
+                }
+                socket?.also {
+//                    manageMyConnectedSocket(it)
+                    mmServerSocket?.close()
+                    shouldLoop = false
+                }
+            }
+        }
+
+        // Closes the connect socket and causes the thread to finish.
+        fun cancel() {
+            try {
+                mmServerSocket?.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "Could not close the connect socket", e)
+            }
+        }
+    }
+
+
+
+    // Client
+    private inner class ConnectThread(device: BluetoothDevice) : Thread() {
+
+        val mmSocket: BluetoothSocket? by lazy(LazyThreadSafetyMode.NONE) {
+            device.createRfcommSocketToServiceRecord(UUID_APP)
+        }
+
+        override fun run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            bluetoothAdapter?.cancelDiscovery()
+
+            mmSocket?.use { socket ->
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                socket.connect()
+
+                // The connection attempt succeeded. Perform work associated with
+                // the connection in a separate thread.
+//                manageMyConnectedSocket(socket)
+            }
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        fun cancel() {
+            try {
+                mmSocket?.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "Could not close the client socket", e)
+            }
+        }
+    }
 }
